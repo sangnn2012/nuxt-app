c690aa50f5ca9caf300a851bebea035b0e49a793
Sang Nguyen <sangnn2012@gmail.com>
2021-03-06 00:42:50 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'OpenSans', sans-serif;
+}
+body {
+  margin: 0
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about-page">
+    <h1>This is my about page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos maxime assumenda quidem reiciendis ducimus quaerat at ratione, obcaecati eligendi, quia repellat quisquam cum. Nulla excepturi enim eos, ut dicta beatae.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos maxime assumenda quidem reiciendis ducimus quaerat at ratione, obcaecati eligendi, quia repellat quisquam cum. Nulla excepturi enim eos, ut dicta beatae.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos maxime assumenda quidem reiciendis ducimus quaerat at ratione, obcaecati eligendi, quia repellat quisquam cum. Nulla excepturi enim eos, ut dicta beatae.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos maxime assumenda quidem reiciendis ducimus quaerat at ratione, obcaecati eligendi, quia repellat quisquam cum. Nulla excepturi enim eos, ut dicta beatae.</p>
+
+  </div>
+</template>
+
+<style scoped>
+  .about-page {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
